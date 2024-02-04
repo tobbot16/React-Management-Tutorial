f4fdc6c01f2b18a2239e20089c2b83a10f845a10
tobbot16 <tobbot16@naver.com>
2024-02-04 16:24:23 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -3,22 +3,55 @@ import './App.css';
 import Customer from './components/Customer';
 
 
-const customer ={
+const customers =[
+  {
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
   'name': '김윤수',
   'birthday': '991121',
   'gender':'남자',
   'job': '대학생'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name': '김윤호',
+  'birthday': '980216',
+  'gender':'남자',
+  'job': '회사원'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name': '심세연',
+  'birthday': '020316',
+  'gender':'여자',
+  'job': '대학생'
 }
+
+]
 
 class App extends Component{
   render(){
   return (
-    <Customer>
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    </Customer>
+    <div>
+      {
+        customers.map(c => {
+          return(
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+          )
+        })
+      }
+
+    </div>
   );
   }
 }
